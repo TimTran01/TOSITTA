2acdb178d41b47ae207e11d0110b499d6092254c
TimTran01 <ttran35112@gmail.com>
2025-11-18 22:19:31 -0500

    Removed pop-up for OCR translation, prints results below the image instead. Image no longer cut off when it is too large. Simplified the language selection code.
    Signed-off-by: TimTran01 <ttran35112@gmail.com>

--- app/src/main/java/com/example/tositta/MainActivity.kt
+++ app/src/main/java/com/example/tositta/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 import android.widget.AdapterView
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.tositta.databinding.ActivityMainBinding
@@ -46,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         //Create Spinner for selecting languages
-        val spinner: Spinner = findViewById(R.id.selectLanguageSpinner)
+        val spinner = binding.selectLanguageSpinner
         //List of supported languages
         val languages = listOf("Latin", "Japanese")
         val adapter = ArrayAdapter(
@@ -57,7 +56,6 @@ class MainActivity : AppCompatActivity() {
         spinner.adapter = adapter
 
         //When selecting a language, set global 'selectLanguage' variable equal to the index of the selected language
-        //TODO: probably a better way to do this so that we're not using magic numbers for OCR else if but it works for now
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -85,66 +83,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun runOCR(bitmap: android.graphics.Bitmap) {
         val image = InputImage.fromBitmap(bitmap, 0)
-        val recognizerLT = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS) //Latin recognizer
-        val recognizerJP = TextRecognition.getClient(JapaneseTextRecognizerOptions.Builder().build()) //Japanese recognizer
-
-        /* Language List
-        Latin = 0
-        Japanese = 1
-        */
-
-        //using global selectedLanguage value, run appropriate text detection
-        //TODO maybe change to when (switch) statement? Android Studio suggests this but cascaded else ifs work for now
-        if (selectedLanguage == 0){ //Latin
-            recognizerLT.process(image)
-                .addOnSuccessListener { result ->
-                    android.app.AlertDialog.Builder(this)
-                        .setTitle("OCR Result")
-                        .setMessage(result.text)
-                        .setPositiveButton("OK", null)
-                        .show()
-                }
-                .addOnFailureListener { e ->
-                    android.app.AlertDialog.Builder(this)
-                        .setTitle("OCR Error")
-                        .setMessage(e.message)
-                        .setPositiveButton("OK", null)
-                        .show()
-                }
-        } else if (selectedLanguage == 1){ // Japanese
-            recognizerJP.process(image)
-                .addOnSuccessListener { result ->
-                    android.app.AlertDialog.Builder(this)
-                        .setTitle("OCR Result")
-                        .setMessage(result.text)
-                        .setPositiveButton("OK", null)
-                        .show()
-                }
-                .addOnFailureListener { e ->
-                    android.app.AlertDialog.Builder(this)
-                        .setTitle("OCR Error")
-                        .setMessage(e.message)
-                        .setPositiveButton("OK", null)
-                        .show()
-                }
-        }  else { //Use latin by default if somehow an invalid value is passed in
-            recognizerLT.process(image)
-                .addOnSuccessListener { result ->
-                    android.app.AlertDialog.Builder(this)
-                        .setTitle("OCR Result")
-                        .setMessage(result.text)
-                        .setPositiveButton("OK", null)
-                        .show()
-                }
-                .addOnFailureListener { e ->
-                    android.app.AlertDialog.Builder(this)
-                        .setTitle("OCR Error")
-                        .setMessage(e.message)
-                        .setPositiveButton("OK", null)
-                        .show()
-                }
+        // Uses Latin by default
+        val recognizer = when (selectedLanguage) {
+            1 -> TextRecognition.getClient(JapaneseTextRecognizerOptions.Builder().build()) // Japanese
+            else -> TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS) // Latin
         }
 
-
+        recognizer.process(image)
+            .addOnSuccessListener { result ->
+                binding.ocrResultTextView.text = result.text
+                binding.ocrResultContainer.visibility = View.VISIBLE
+            }
+            .addOnFailureListener { e ->
+                binding.ocrResultTextView.text = "OCR Error: ${e.message}"
+                binding.ocrResultContainer.visibility = View.VISIBLE
+            }
     }
 }
